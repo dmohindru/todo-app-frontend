f935ecdf096865e9ce04e8ca94d726fa7cf6c83c
Dhruv Mohindru <dmohindru@gmail.com>
2023-12-20 07:37:02 +1100

    making progress in navbar construction

---
+++ src/component/LandingPageNavbar.tsx
@@ -0,0 +1,12 @@
+import { Home, DarkMode } from "@mui/icons-material";
+import { AppBar, Typography, Box, Button } from "@mui/material";
+
+// interface NavbarProps {
+//   authenticated: boolean;
+// }
+
+const LandingPageNavbar: React.FC = () => {
+  return <AppBar position="sticky"></AppBar>;
+};
+
+export default LandingPageNavbar;
