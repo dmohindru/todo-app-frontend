26a6e76e96c8c12a1f4059f491c9b0c8c9c90ea0
Dhruv Mohindru <dmohindru@gmail.com>
2024-01-08 17:18:28 +1100

    Working on keycloak integration for this app

---
+++ src/auth/Keycloak-config.ts
@@ -0,0 +1,1 @@
+import Keycloak from "keycloak-js";
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,45 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Keycloak from "keycloak-js";
+
+/*
+Init Options
+*/
+
+let initOptions = {
+  url: "http://localhost:8180",
+  realm: "todoapp",
+  clientId: "todo-react-client",
+};
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: "login-required",
+  checkLoginIframe: true,
+  pkceMethod: "S256",
+}).then(
+  (auth) => {
+    console.log("keycloak login auth object", auth);
+    if (!auth) {
+      window.location.reload();
+    } else {
+      // successful authentication
+      console.log("Authentication");
+      console.log("auth", auth);
+      console.log("Keycloak", kc);
+      console.log("Access Token", kc.token);
+
+      kc.onTokenExpired = () => {
+        console.log("token expire");
+      };
+    }
+  },
+  () => {
+    console.log("Authentication failed");
+  }
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
